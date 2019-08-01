8a46f09ef881fa3a435afa3c744f963d0346807c
ian mulvany <ian@mulvany.net>
2019-08-01 20:07:49 +0100

    first pass at creating utility conversion function

---
+++ convert_source_to_abstract_post.py
@@ -0,0 +1,19 @@
+"""
+take input from:
+    - locally saved hugo bear
+    - data pulled from pinboard
+
+turn this source info into an abstract representation of a blog post
+"""
+
+from blog_post import BlogPost
+
+HugoPath = "path_to_bear_posts"
+
+def bear_to_post(path_to_bear_post):
+    blob_post_tmp = "this will be a blog post object"
+    return blob_post_tmp
+
+def pinboard_to_post(path_to_bear_post):
+    pinboard_post_tmp = "this will be a blog post object"
+    return pinboard_post_tmp
