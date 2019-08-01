e97dc41c0e76f9a2b7effccdfbd8cf6597f41383
ian mulvany <ian@mulvany.net>
2019-08-01 23:26:23 +0100

    simplify class instantiation

--- hugo_post.py
+++ hugo_post.py
@@ -2,14 +2,14 @@
 import datetime
 today = datetime.date.today()
 
-class HugoPost:
-    def __init__(self, title, tags, body):
-        self.hugo_title = title
-        self.input_tags = tags
-        self.hugo_tags = self.filter_tags(self.input_tags)
-        self.hugo_body = body
-        self.hugo_meta = self.generate_hugo_metadata(self.hugo_title, self.hugo_tags)
+class HugoPost(BlogPost):
+    def __init__(self, blog_post):
+        BlogPost.__init__(self, blog_post.title, blog_post.input_tags, blog_post.body)
+        # super().__init__(self.post_title, self.post_tags, self.post_body)
+        self.hugo_body = self.body
+        self.hugo_meta = self.generate_hugo_metadata(self.title, self.output_tags)
         self.hugo_post = self.hugo_meta + "\n" + self.hugo_body
+        self.hugo_post_filename = self.hugo_post_filename(self.title)
 
     @staticmethod
     def generate_hugo_metadata(title, tags):
